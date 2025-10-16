01b58b3376964ef9083226c5063a36997465d3e7
Kaif <Kaifm3742@gmail.com>
2025-10-16 23:34:39 +0530

    Added number guessing game with input validation

--- NumberGuessingGame.py
+++ NumberGuessingGame.py
@@ -1,1 +1,22 @@
-i dont knwo
+import time
+print("<-/Welcome to the Number Guessing Game\\->")
+while True:
+ 
+ 
+ Random_Number = int(time.strftime("%M")) % 100
+ try:
+  UserInput = int(input("Enter the Number You Guessed : "))
+ except ValueError:
+    print("Oops! That wasn't a number. Try again.")
+    continue
+ 
+ if UserInput == Random_Number:
+     print("You Guessed the Correct Number\n")
+     break
+ elif UserInput > Random_Number:
+     print("Your Number is Greater the Actual Number\n")
+ elif UserInput < Random_Number:
+     print("The Guessed Number is Lower Then The Actual Number\n")
+ else:
+     print("Its Impossible to Even Trigger This Congrats on How You did it")
+    

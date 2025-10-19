11f39f0c75989e222d7d181248f60a5413014d0d
Kaif <Kaifm3742@gmail.com>
2025-10-20 04:55:23 +0530

    Tried New Things

--- starting.py
+++ starting.py
@@ -1,21 +1,28 @@
-operation = input("Choose an arithmetic operation (+, -, /, %): ")
-
-if operation == '+':
-    Num = float(input("Enter the first number: "))
-    Num2 = float(input("Enter the second number: "))
-    print("There you go:", Num + Num2)
-elif operation == '-':
-    Num = float(input("Enter the first number: "))
-    Num2 = float(input("Enter the second number: "))
-    print("There you go:", Num - Num2)
-elif operation == '/':
-     Num = float(input("Enter the first number: "))
-     Num2 = float(input("Enter the second number: "))
-     print("There you go:", Num / Num2)
-elif operation == '%':
-    Num = float(input("Enter the first number: "))
-    Num2 = float(input("Enter the second number: "))
-    print("There you go:", Num % Num2)
-else:
-    print("\nThis parameter is incorrect.")
-
+while True:
+    try:
+         operation = input("\nChoose an arithmetic operation (+, -, /, %) or 'quit' to Exit: ")
+         if operation.lower() == 'quit':
+            break
+    except ValueError:
+        print("This Operator is Incorrect.")
+        continue
+    if operation == '+':
+         Num = float(input("Enter the first number: "))
+         Num2 = float(input("Enter the second number: "))
+         print("There you go:", Num + Num2)
+    elif operation == '-':
+         Num = float(input("Enter the first number: "))
+         Num2 = float(input("Enter the second number: "))
+         print("There you go:", Num - Num2)
+    elif operation == '/':
+          Num = float(input("Enter the first number: "))
+          Num2 = float(input("Enter the second number: "))
+          print("There you go:", Num / Num2)
+    elif operation == '%':
+         Num = float(input("Enter the first number: "))
+         Num2 = float(input("Enter the second number: "))
+         print("There you go:", Num % Num2)
+    else:
+         print("\nThis Operator is incorrect.")
+ 
+ 
